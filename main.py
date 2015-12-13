53009c0268dfd79f9b6b313021ef87d4dbde0e89
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-12-13 23:22:01 +0200

    read steps from config.yaml and print intructions with colors.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
+import yaml
 
-user_input = raw_input('cloudify>')
-while True:
-    os.system(user_input)
-    user_input = raw_input('cloudify>')
+config_stream = open('config.yaml', 'r')
+config = yaml.load(config_stream)
+user_input='start'
+steps = config['steps']
+
+for step in steps:
+    print '\033[92m' + step['intro'].format('\033[94m' + step['command'] + '\033[0m \033[92m') + '\033[0m'
+    while str(user_input.strip()) !=  str(step['command'].strip()):
+
+        user_input = raw_input('cfy>')
+        os.system(user_input)
+
 
 # print 'hello ' + console_output
 
