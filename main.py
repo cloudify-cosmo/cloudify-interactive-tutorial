57dc6655a70fd353358388c605f0f4f26ae75a73
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-12-30 18:52:23 +0200

    add random port support

--- main.py
+++ main.py
@@ -3,44 +3,111 @@
 import os
 import yaml
 import shutil
+import atexit
+import socket
+import subprocess
+import re
 
 
-my_path = os.path.realpath(__file__)
-my_dir = os.path.dirname(my_path)
-config_stream = open( my_dir + '/config.yaml', 'r')
-config = yaml.load(config_stream)
+class Data:
+    my_path = os.path.realpath(__file__)
+    my_dir = os.path.dirname(my_path)
+    port = None
 
-port_file = os.path.join( my_dir, 'port')
-last_port = int(open(port_file,'r').read())
-new_port = last_port + 1
-open(port_file,'w').write(str(new_port))
+    def __init__(self):
+        pass
 
 
-# change base dir to be with blueprint
-if config['base_dir'] != '__base_dir__':
-    os.chdir(config['base_dir'])
-else:
-    ''' go to default location '''
-    os.chdir('dev/my_blueprint')
-
-# setup a new workspace for this user with the new port
-cwd = os.getcwd()
-new_cwd = os.path.join(my_dir, 'dev','workspace','blueprint_' + str(new_port))
-shutil.copytree(cwd, new_cwd)
-os.chdir(new_cwd)
-
-user_input='start'
-steps = config['steps']
-
-for step in steps:
-    step['command'] = step['command'].replace('__port__',str(new_port))
-    print '\033[92m' + step['intro'].format('\033[94m' + step['command'] + '\033[0m \033[92m') + '\033[0m'
-    while str(user_input.strip()) !=  str(step['command'].strip()):
-
-        user_input = raw_input('cfy>')
-        os.system(user_input)
+data = Data()
 
 
+# http://stackoverflow.com/questions/1365265/on-localhost-how-to-pick-a-free-port-number
+def get_free_port():
+    sock = socket.socket()
+    sock.bind(('', 0))
+    result = sock.getsockname()[1]
+    sock.close()
+    return result
+
+
+def get_workspace_folder():
+    return os.path.join( data.my_dir, 'dev', 'workspace', 'blueprint_' + str(data.port))
+
+
+def create_workspace_folder():
+    # setup a new workspace for this user with the new port
+    cwd = os.getcwd()
+    new_cwd = get_workspace_folder()
+    shutil.copytree(cwd, new_cwd)
+    os.chdir(new_cwd)
+
+
+def delete_workspace_folder():
+    path = get_workspace_folder()
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+
+
+# http://stackoverflow.com/questions/14907766/kill-program-listening-to-a-given-port
+def kill_process_by_port():
+
+    ports = [str(data.port)]
+
+    popen = subprocess.Popen(['netstat', '-lpn'],
+                             shell=False,
+                             stdout=subprocess.PIPE)
+    (output, err) = popen.communicate()
+
+    pattern = "^tcp.*((?:{0})).* (?P<pid>[0-9]*)/.*$"
+    pattern = pattern.format(')|(?:'.join(ports))
+    prog = re.compile(pattern)
+    for line in output.split('\n'):
+        match = re.match(prog, line)
+        if match:
+            pid = match.group('pid')
+            subprocess.Popen(['kill', '-9', pid])
+
+
+
+def main():
+
+    config_stream = open( data.my_dir + '/config.yaml', 'r')
+    config = yaml.load(config_stream)
+
+    data.port = get_free_port()
+
+    # change base dir to be with blueprint
+    if config['base_dir'] != '__base_dir__':
+        os.chdir(config['base_dir'])
+    else:
+        ''' go to default location '''
+        os.chdir('dev/my_blueprint')
+
+    create_workspace_folder( )
+
+    user_input='start'
+    steps = config['steps']
+
+    for step in steps:
+        step['command'] = step['command'].replace('__port__',str( data.port ))
+        print '\033[92m' + step['intro'].format('\033[94m' + step['command'] + '\033[0m \033[92m') + '\033[0m'
+        while str(user_input.strip()) !=  str(step['command'].strip()):
+
+            user_input = raw_input('cfy>')
+            os.system(user_input)
+
+def cleanup():
+    kill_process_by_port()
+    delete_workspace_folder()
+
+
+try:
+    main()
+except KeyboardInterrupt:
+    cleanup()
+
+
+atexit.register(cleanup)
 # print 'hello ' + console_output
 
 
