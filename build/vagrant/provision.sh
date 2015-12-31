4af92e5c98fddc04e2367932abcf2e2a75339bdd
nir0s <nir36g@gmail.com>
2015-12-31 21:59:44 +0200

    prvision scripts refactoring. not done yet

--- build/vagrant/provision.sh
+++ build/vagrant/provision.sh
@@ -1,56 +1,22 @@
 #!/bin/bash
 
 PROJECT_NAME="cloudify-interactive-tutorial"
-PROJECT_HOME="`pwd`/$PROJECT_NAME"
+PROJECT_HOME="`pwd`/$PROJECT_NAME/tutorials/intro"
+
+#need this for machine provisioning.. but not necessarily for local
+#     sudo apt-get update -y
+#     sudo apt-get install git python-pip -y --fix-missing
+#     sudo apt-get install build-essential python-dev unzip  -y
 
 sudo pip install cloudify
 git clone https://github.com/cloudify-cosmo/${PROJECT_NAME}.git
 cd $PROJECT_NAME
 sudo pip install 3rd-parties/butterfly
-curl -L -O https://github.com/cloudify-examples/simple-python-webserver-blueprint/archive/master.tar.gz`
-untar -xzvf master.tar.gz
-butterfly.server.py --unsecure --host=0.0.0.0 --port=8088 --login=False --shell=$PROJECT_HOME/main.py
-
-# if [ ! -e $PROJECT_HOME ];then
-#     sudo apt-get update -y
-#     sudo apt-get install git python-pip -y --fix-missing
-#     sudo apt-get install build-essential python-dev unzip  -y
-
-#     git clone https://github.com/cloudify-cosmo/${PROJECT_NAME}.git
-
-#     sudo pip install cloudify
-# else
-#     echo "assuming installed"
-# fi
-
-# rm -rf my_blueprint # clean
-# mkdir my_blueprint
-# pushd my_blueprint
-#     wget https://github.com/cloudify-examples/simple-python-webserver-blueprint/archive/master.zip
-#     unzip master.zip
-#     cd simple-python-webserver-blueprint-master
-#     export BLUEPRINT_HOMEDIR=`pwd`
-#     sed -i.bak s#__base_dir__#$BLUEPRINT_HOMEDIR#g $PROJECT_HOME/config.yaml
-# popd
-
-# pushd $PROJECT_HOME
-
-#     mkdir -p dev/workspace
-
-#     pushd 3rd-parties/butterfly
-#         sudo pip install .
-#     popd
-
-#     nohup butterfly.server.py --unsecure --host=0.0.0.0 --port=8088 --login=False --shell=$PROJECT_HOME/main.py &> /dev/null &
-
-
-#     pushd static
-
-#         nohup ./server.sh &
-
-#     popd
-
-# popd
+cd $PROJECT_HOME
+curl -L -O https://github.com/cloudify-examples/simple-python-webserver-blueprint/archive/master.tar.gz
+tar -xzvf master.tar.gz
+cd simple-python-webserver-blueprint-master
+nohup butterfly.server.py --unsecure --host=0.0.0.0 --port=8088 --login=False --shell=$PROJECT_HOME/main.py & > /dev/null
 
 
 export MY_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
